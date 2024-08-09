ddf987a9b04b83c582ab18f23dfa415b587cd843
tien <nq2017.nguyentien081202@gmail.com>
2024-08-09 16:26:00 +0700

    Add Diaries Entity

---
+++ OnlineDiary/Models/Diary.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineDiary.Models
+{
+    [Table("diaries")]
+    public class Diary
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Posted { get; set; }
+    }
+}
+}
