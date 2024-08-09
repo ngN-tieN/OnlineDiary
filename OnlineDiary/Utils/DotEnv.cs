5fe8dc8c9a60f9d9273af714918d7810b5f0e9a2
tien <nq2017.nguyentien081202@gmail.com>
2024-08-09 16:28:42 +0700

    Utils to get secret key from .env files

---
+++ OnlineDiary/Utils/DotEnv.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineDiary.Utils
+{
+    using System;
+    using System.IO;
+    public class DotEnv
+    {
+        public static void Load(string path)
+        {
+            if (!File.Exists(path))
+                return;
+            foreach (var line in File.ReadAllLines(path))
+            {
+                var parts = line.Split('=', 2, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine(parts[0], parts[1]);
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
+        }
+    }
+}
---
+++ OnlineDiary/Utils/GetEnvVar.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineDiary.Utils
+{
+    public class GetEnvVar
+    {
+        public static string GetEnvString(string key)
+        {
+            var root = Directory.GetCurrentDirectory();
+            var dotenv = Path.Combine(root, ".env");
+            DotEnv.Load(dotenv);
+            return Environment.GetEnvironmentVariable("MSSQL_CONNECTION_STRING");
+        }
+    }
+}
