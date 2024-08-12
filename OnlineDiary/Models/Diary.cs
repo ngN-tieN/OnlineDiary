9a56223eca7772f41145453ab16e7282dbb6678d
tien <nq2017.nguyentien081202@gmail.com>
2024-08-12 17:57:19 +0700

    Update more properties and attributes in Diary.cs

--- OnlineDiary/Models/Diary.cs
+++ OnlineDiary/Models/Diary.cs
@@ -1,21 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineDiary.Models
 {
     [Table("diaries")]
     public class Diary
     {
-        [Key]
-        public string DiaryId { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime Posted { get; set; }
-
-        //1 diary page belong to 1 user 
-        public string UserId { get; set; }
-        [ForeignKey(nameof(UserId))]
-        public User User { get; set; }  
     }
 }
-
+}
