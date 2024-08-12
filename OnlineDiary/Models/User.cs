7cb3a997bc8f492f52f6a102ed995ce69b277e1c
tien <nq2017.nguyentien081202@gmail.com>
2024-08-12 18:29:52 +0700

    Add properties and attributes to User.cs

--- OnlineDiary/Models/User.cs
+++ OnlineDiary/Models/User.cs
@@ -1,7 +1,20 @@
-﻿namespace OnlineDiary.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineDiary.Models
 {
+    [Table("users")]
     public class User
     {
+        [Key]
+        public string UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string Password { get; set; }
 
+        public List<Diary> Diaries { get; set; }
     }
 }
